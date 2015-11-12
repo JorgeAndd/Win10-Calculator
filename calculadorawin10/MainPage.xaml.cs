612fb66b7b7fbf89ccbf1a9973e7fc576f77aa8d
Jorgel <jorgeluizandrade@outlook.com>
2015-11-12 21:48:27 -0200

    Initial commit. Interface done

---
+++ calculadorawin10/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using calculadorawin10.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace calculadorawin10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            CalculadoraVO calculadora = new CalculadoraVO
+            {
+                Numero1 = int.Parse(txtNumero1.Text),
+                Numero2 = int.Parse(txtNumero2.Text)
+            };
+
+            txtResultado.Text = calculadora.Somar().ToString();
+        }
+    }
+}
---
+++ calculadorawin10/VO/CalculadoraVO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadorawin10.VO
+{
+    public class CalculadoraVO
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+
+        public double Somar()
+        {
+            return Numero1 + Numero2;
+        }
+    }
+}
